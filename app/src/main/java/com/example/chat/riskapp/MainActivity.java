d7e2f963b852b8008adb0c3dd25b05c5c1e5d560
Liozart <bobponay@gmail.com>
2017-03-16 18:00:08 +0100

    socket connect + Minimal UI

--- app/src/main/java/com/example/chat/riskapp/MainActivity.java
+++ app/src/main/java/com/example/chat/riskapp/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.chat.riskapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
 import android.app.Dialog;
@@ -12,14 +11,34 @@ import android.content.Intent;
 import android.content.DialogInterface;
 import android.graphics.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.net.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Socket socket;
+    public static final int SERVERPORT = 666;
+    public static final String SERVER_IP = "0.0.0.0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try { ConnectToServ(); }
+        catch (IOException e1) {
+            e1.printStackTrace(); }
+    }
+
+    public void ConnectToServ() throws IOException{
+        try
+        {
+            InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+            socket = new Socket(serverAddr, SERVERPORT);
+
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
     }
 }
