5f4de99aa167ab0429c79ff7739165a3cb473d6d
Liozart <bobponay@gmail.com>
2017-03-16 09:41:22 +0100

    Initial commit

---
+++ app/src/main/java/com/example/chat/riskapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.chat.riskapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Display;
+import android.app.Dialog;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.DialogInterface;
+import android.graphics.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
